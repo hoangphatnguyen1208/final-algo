56628bb67aaf7d8cf9d6ac0310f164429bfac575
Nguyen Hoang Phat <88542260+hoangphat120805@users.noreply.github.com>
2026-05-13 21:37:03 +0700

    Refactor merge_sort function and add .gitignore file

--- problem4.cpp
+++ problem4.cpp
@@ -4,18 +4,42 @@ using namespace std;
 
 const int MAXN = 50005;
 int n;
-int a[MAXN];
+int a[MAXN], tmp[MAXN];
 
-long long merge_sort(int l, int r) {
-    if (l >= r) return 0;
+long long merge_sort(int left, int right) {
+    if (left >= right) return 0;
 
-    int mid = l + r >> 1;
+    int mid = left + right >> 1;
     long long ans = 0;
-    ans += merge_sort(l, mid);
-    ans += merge_sort(mid+1, r);
+    ans += merge_sort(left, mid);
+    ans += merge_sort(mid+1, right);
 
+    int i = left;
+    int j = mid + 1;
+    int k = left;
     
+    while (i <= mid && j <= right) {
+        if (a[i] <= a[j]) {
+            tmp[k++] = a[i++];
+        }
+        else {
+            tmp[k++] = a[j++];
+            ans += mid-i+1;
+        }
+    }
 
+    while (i <= mid) {
+        tmp[k++] = a[i++];
+    }
+
+    while (j <= right) {
+        tmp[k++] = a[j++];
+    }
+
+    for (int p = left; p <= right; p++) {
+        a[p] = tmp[p];
+    }
+    return ans;
 }
 
 int main() {
@@ -24,6 +48,6 @@ int main() {
         cin >> a[i];
     }
 
-
+    cout << merge_sort(1, n);
     return 0;
 }
