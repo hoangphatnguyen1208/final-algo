731830ddc512f65e8fcb3eee50da50a4cc535def
Nguyen Hoang Phat <88542260+hoangphat120805@users.noreply.github.com>
2026-05-13 22:40:34 +0700

    Add implementation of a priority queue-based merging algorithm in problem6.cpp

---
+++ problem6.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int k;
+vector <vector<int>> a;
+
+class Node {
+    public:
+        int val;
+        int idx;
+        int row;
+
+    bool operator> (const Node &other) const {
+        return val > other.val;
+    }
+};
+
+int main() {
+    cin >> k;
+    cin.ignore();
+    a.assign(k, vector<int>(0));
+    for (int i = 0; i < k; i++) {
+        string line;
+        getline(cin, line);
+
+        stringstream ss(line);
+        int x;
+        while (ss >> x) {
+            a[i].push_back(x);
+        }
+    }
+
+    priority_queue<Node, vector<Node>, greater<Node>> pq;
+
+    for (int i = 0; i < k; i++) {
+        pq.push({a[i][0], 0, i});
+    }
+    while (pq.size() != 0) {
+        Node cur = pq.top();
+        pq.pop();
+        cout << cur.val << ' ';
+        int idx = cur.idx;
+        int row = cur.row;
+        if (idx != a[row].size() - 1) {
+            pq.push({a[row][idx + 1], idx + 1, row});
+        }
+    }
+    return 0;
+}
